9a2a0e404f02d7579d5ee97f9d7fed6e596e97ab
thiyaneswar gunasekaran <thiyaneswar99@gmail.com>
2021-09-16 13:18:28 +0530

    Commit one -- Basic App and Entry point

---
+++ Mundus/src/Mundus.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+// To link the Mundus to the CLIENT
+#include "Mundus/Application.h"
+
+// To create an ENTRY POINT------------------
+#include "Mundus/EntryPoint.h"
+// ------------------------------------------
